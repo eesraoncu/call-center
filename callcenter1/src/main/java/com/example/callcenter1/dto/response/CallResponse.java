a0c81652c72a76f306304fffa79a7c54d174ea09
Hikmet <94120024+hikmetdev@users.noreply.github.com>
2025-07-07 17:17:13 +0300

    Update CallResponse.java

--- callcenter1/src/main/java/com/example/callcenter1/dto/response/CallResponse.java
+++ callcenter1/src/main/java/com/example/callcenter1/dto/response/CallResponse.java
@@ -1,4 +1,12 @@
 package com.example.callcenter1.dto.response;
 
+import lombok.Data;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
 public class CallResponse {
+    private Integer callId;
+    private LocalTime callTime;
+    private LocalDate callDate;
 }
