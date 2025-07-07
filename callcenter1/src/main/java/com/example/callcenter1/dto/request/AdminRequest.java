a8f9d64ac2f82913e26a0d02347d4e0e640934b6
Hikmet <94120024+hikmetdev@users.noreply.github.com>
2025-07-07 17:14:34 +0300

    Update AdminRequest.java

--- callcenter1/src/main/java/com/example/callcenter1/dto/request/AdminRequest.java
+++ callcenter1/src/main/java/com/example/callcenter1/dto/request/AdminRequest.java
@@ -1,4 +1,14 @@
 package com.example.callcenter1.dto.request;
 
+import lombok.Data;
+
+@Data
 public class AdminRequest {
+    private Integer operatorId;
+    private Integer logId;
+    private Integer roleId;
+    private String adminName;
+    private String adminSurname;
+    private String adminEmail;
+    private String adminPassword;
 }
