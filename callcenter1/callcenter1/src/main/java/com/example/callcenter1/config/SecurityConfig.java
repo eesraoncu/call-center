8ba60f1787da301cf31ef7509e5cd2b40ebfc82d
Mustafa Bosnalı <mustafabosnali03@gmail.com>
2025-07-08 17:54:54 +0300

    Update SecurityConfig.java

--- callcenter1/callcenter1/src/main/java/com/example/callcenter1/config/SecurityConfig.java
+++ callcenter1/callcenter1/src/main/java/com/example/callcenter1/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/register", "/swagger-ui/**", "/v3/api-docs/**").permitAll()//burada tanımlanan endpointler dışarıdan erişilebilir.
+                        .requestMatchers("/login", "/register", "/swagger-ui/**", "/v3/api-docs/**", "/api/calls/**", "/api/customers/**", "/api/operators/**", "/api/logs/**", "/api/roles/**", "/api/admins/**", "/api/services/**", "/api/locations/**").permitAll()//burada tanımlanan endpointler dışarıdan erişilebilir.
                         .requestMatchers("/demo-verify-int-test", "/demo-verify-int").authenticated() // Demo endpoint'leri için authentication gerekli
                         .anyRequest().authenticated()
                 )
