ecafb1fdcfd6871fd0eaae306d41884a24ac3445
Hikmet <94120024+hikmetdev@users.noreply.github.com>
2025-07-08 16:14:59 +0300

    Update OperatorRequest.java

--- callcenter1/callcenter1/src/main/java/com/example/callcenter1/dto/request/OperatorRequest.java
+++ callcenter1/callcenter1/src/main/java/com/example/callcenter1/dto/request/OperatorRequest.java
@@ -1,5 +1,12 @@
 package com.example.callcenter1.dto.request;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OperatorRequest {
     private String operatorName;
     private String operatorSurname;
@@ -7,33 +14,4 @@ public class OperatorRequest {
     private String operatorEmail;
     private String operatorPassword;
     private Integer roleId;
-
-    public OperatorRequest() {}
-
-    public OperatorRequest(String operatorName, String operatorSurname, String operatorPhoneNumber, String operatorEmail, String operatorPassword, Integer roleId) {
-        this.operatorName = operatorName;
-        this.operatorSurname = operatorSurname;
-        this.operatorPhoneNumber = operatorPhoneNumber;
-        this.operatorEmail = operatorEmail;
-        this.operatorPassword = operatorPassword;
-        this.roleId = roleId;
-    }
-
-    public String getOperatorName() { return operatorName; }
-    public void setOperatorName(String operatorName) { this.operatorName = operatorName; }
-
-    public String getOperatorSurname() { return operatorSurname; }
-    public void setOperatorSurname(String operatorSurname) { this.operatorSurname = operatorSurname; }
-
-    public String getOperatorPhoneNumber() { return operatorPhoneNumber; }
-    public void setOperatorPhoneNumber(String operatorPhoneNumber) { this.operatorPhoneNumber = operatorPhoneNumber; }
-
-    public String getOperatorEmail() { return operatorEmail; }
-    public void setOperatorEmail(String operatorEmail) { this.operatorEmail = operatorEmail; }
-
-    public String getOperatorPassword() { return operatorPassword; }
-    public void setOperatorPassword(String operatorPassword) { this.operatorPassword = operatorPassword; }
-
-    public Integer getRoleId() { return roleId; }
-    public void setRoleId(Integer roleId) { this.roleId = roleId; }
 }
