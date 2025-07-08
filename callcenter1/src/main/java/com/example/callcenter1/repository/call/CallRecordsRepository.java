544fec6a7c7e44cbb7063a79661775691ed3ebf3
AhmetYunusDemirci <213655046+AhmetYunusDemirci@users.noreply.github.com>
2025-07-08 10:33:14 +0300

    Update CallRecordsRepository.java

--- callcenter1/src/main/java/com/example/callcenter1/repository/call/CallRecordsRepository.java
+++ callcenter1/src/main/java/com/example/callcenter1/repository/call/CallRecordsRepository.java
@@ -1,4 +1,29 @@
 package com.example.callcenter1.repository.call;
 
-public class CallRecordsRepository {
+import com.example.callcenter1.model.call.CallRecords;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CallRecordsRepository extends JpaRepository<CallRecords, Long> {
+    // Örneğin: Belirli bir tarihteki çağrıları getir
+    List<CallRecords> findByCallDate(String callDate);
+
+    // Çağrı saatine göre filtreleme
+    List<CallRecords> findByCallTime(String callTime);
+    // Çağrı durumuna göre filtreleme
+    List<CallRecords> findByCallStatus(String callStatus);
+    // Çağrı türüne göre filtreleme
+    List<CallRecords> findByCallType(String callType);
+    // Çağrı yönlendirme durumuna göre filtreleme
+    List<CallRecords> findByCallForwarded(Boolean callForwarded);
+    // Çağrı yönlendirme numarasına göre filtreleme
+    List<CallRecords> findByForwardedNumber(String forwardedNumber);
+    // Çağrı yönlendirme tarihine göre filtreleme
+    List<CallRecords> findByForwardedDate(String forwardedDate);    
+    // Çağrı yönlendirme saatine göre filtreleme
+    List<CallRecords> findByForwardedTime(String forwardedTime);    
+    
 }
